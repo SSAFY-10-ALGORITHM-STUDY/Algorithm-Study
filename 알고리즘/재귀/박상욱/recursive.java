255bb1deeb250c3a8d73ad50ad1390141ae27c7f
박상욱 <dkxkqkrtkddn@naver.com>
2024-01-23 17:43:57 +0900

    Feat: commit Test

---
+++ 알고리즘/재귀/박상욱/recursive.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static int ans, N, K, arr[],temp;
+	
+	public static void recursive(int num) {
+		for (int i = 0; i < K; i++) {
+			temp = num * 10 + arr[i];
+			if(temp<=N) {
+				ans = (ans>temp?ans:temp);
+				recursive(temp);
+			}
+		}
+	}
+
+	public static void init() throws NumberFormatException, IOException {
+		ans = 0;
+		
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+
+		arr = new int[K];
+
+		st = new StringTokenizer(in.readLine());
+
+		for (int i = 0; i < K; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+	}
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		init();
+		recursive(0);
+		System.out.println(ans);
+	}
+}
