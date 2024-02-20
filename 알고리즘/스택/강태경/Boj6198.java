db4caf1efcf84d3c194cd91fe447ca1ef41c5184
Kang Taegyeong <94584208+ktk3939@users.noreply.github.com>
2024-02-20 10:07:31 +0900

    [강태경]: 2개의 q 사용, 큰 건물 만나면 pop, 작은 건물 만나면 스택사이즈만큼 +

---
+++ 알고리즘/스택/강태경/Boj6198.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int n;
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		n = Integer.parseInt(br.readLine());
+		Deque<Long> q = new ArrayDeque<>();
+		Deque<Long> q2 = new ArrayDeque<>();
+		for (int i = 0; i < n; i++) {
+			q.addFirst(Long.parseLong(br.readLine()));
+		}
+		q2.add(q.removeLast());
+		long answer = 0L;
+		long buf = 1L;
+		while (!q.isEmpty()) {
+			long first = q2.removeLast(); // 12 13 14
+			long second = q.removeLast(); // 2
+			if (first > second) {
+				q2.add(first);
+				q2.add(second);
+				answer += buf;
+				buf++;
+			} else {
+				buf--;
+				if (q2.isEmpty()) {
+					q2.add(second);
+					buf=1L;
+				} else {
+					q.add(second);
+				}
+			}
+		}
+
+		System.out.println(answer);
+	}
+
+}
