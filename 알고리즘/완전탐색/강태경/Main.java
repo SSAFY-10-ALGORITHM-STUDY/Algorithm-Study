964bb2f9ffcf5108494de1703158db8b7950caf2
Kang Taegyeong <94584208+ktk3939@users.noreply.github.com>
2024-02-20 09:36:30 +0900

    [강태경]: 모든 가능한 공차로 완전탐색, 완전제곱수 판단

--- 알고리즘/완전탐색/강태경/Main.java
+++ 알고리즘/완전탐색/강태경/Main.java
@@ -1,114 +1,54 @@
 package backjoon;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
+	static int n, m;
+	static int answer = -1;
+	static int[][] arr;
 
-	static int n;
-	static int[] arr;
-	static boolean[] visit;
-	static ArrayList<ArrayList<Integer>> list;
-
-	static void combination(int[] arr, boolean[] visit, int start, int n, int r) {
-		if (r == 0) {
-			ArrayList<Integer> temp = new ArrayList<>();
-			for (int i = 1; i <= n; i++) {
-				if (visit[i])
-					temp.add(i);
-			}
-			list.add(temp);
-			return;
+	static void find(int x, int y, int dx, int dy) {
+		int buf = 0;
+		while (x >= 0 && x < n && y >= 0 && y < m) {
+			buf = buf * 10 + arr[x][y];
+			if (isSquare(buf))
+				answer = Math.max(answer, buf);
+			if(dx==0&&dy==0) break;
+			x += dx;
+			y += dy;
 		}
-
-		for (int i = start; i <= n; i++) {
-			visit[i] = true;
-			combination(arr, visit, i + 1, n, r - 1);
-			visit[i] = false;
-		}
-
 	}
 
-	public static void main(String[] args) throws IOException {
+	static boolean isSquare(int n) {
+		return Math.pow((int)(Math.pow(n, 0.5)), 2) == n;
+	}
+
+	public static void main(String[] args) throws Exception {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		arr = new int[n + 1];
-		for (int i = 1; i <= n; i++) {
-			arr[i] = i;
-		}
-		list = new ArrayList<>();
 		StringTokenizer stk = new StringTokenizer(br.readLine());
-		int mp = Integer.parseInt(stk.nextToken());
-		int mf = Integer.parseInt(stk.nextToken());
-		int ms = Integer.parseInt(stk.nextToken());
-		int mv = Integer.parseInt(stk.nextToken());
-		Map<Integer, ArrayList<Integer>> map = new HashMap<>();
-		int p = 0, f = 0, s = 0, v = 0, cost = 0;
-		for (int i = 1; i <= n; i++) {
-			stk = new StringTokenizer(br.readLine());
-			p = Integer.parseInt(stk.nextToken());
-			f = Integer.parseInt(stk.nextToken());
-			s = Integer.parseInt(stk.nextToken());
-			v = Integer.parseInt(stk.nextToken());
-			cost = Integer.parseInt(stk.nextToken());
-			map.put(i, new ArrayList<>(List.of(cost, p, f, s, v)));
-		}
-		for (int i = 1; i <= n; i++) {
-			visit = new boolean[n + 1];
-			combination(arr, visit, 1, n, i);
-		}
-		ArrayList<ArrayList<Integer>> answer = null;
-		ArrayList<Integer> temp = null;
-		int lowCost = 10000;
-		int np, nf, ns, nv, ncost;
-		for (ArrayList<Integer> comb: list) {
-			np =nf =ns = nv = ncost =0;
-			for (int idx : comb) {
-				temp = map.get(idx);
-				ncost+=temp.get(0);
-				np+=temp.get(1);
-				nf+=temp.get(2);
-				ns+=temp.get(3);
-				nv+=temp.get(4);
-			}
-			if (np>=mp && nf>=mf && ns>=ms && nv>=mv) {
-//				System.out.println(comb);
-				if (ncost<lowCost) {
-					lowCost = ncost;
-					answer = new ArrayList<>();
-					answer.add(comb);
-				} else if(ncost==lowCost) {
-					answer.add(comb);
-				}
+		StringBuilder sb = new StringBuilder();
+		n = Integer.parseInt(stk.nextToken());
+		m = Integer.parseInt(stk.nextToken());
+		arr = new int[n][m];
+		for (int i = 0; i < n; i++) {
+			String str = br.readLine();
+			for (int j = 0; j < m; j++) {
+				arr[i][j] = str.charAt(j)-'0';
 			}
 		}
-		if(answer==null) System.out.println(-1);
-		else {
-			System.out.println(lowCost);
-			Collections.sort(answer, new Comparator<ArrayList<Integer>>() {
-				@Override
-				public int compare(ArrayList<Integer> o1, ArrayList<Integer> o2) {
-					for (int i = 0; i < Math.min(o1.size(), o2.size()); i++) {
-						int compareResult = o1.get(i).compareTo(o2.get(i));
-						if(compareResult!=0) {
-							return compareResult;
-						}
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				for (int k = -8; k <= 8; k++) {
+					for (int l = -8; l <= 8; l++) {
+						find(i, j, k, l);
 					}
-					return Integer.compare(o1.size(), o2.size());
 				}
-			});
-			for (int num : answer.get(0)) {
-				System.out.print(num+" ");
 			}
 		}
+		sb.append(answer);
+		System.out.println(sb);
 	}
 }
