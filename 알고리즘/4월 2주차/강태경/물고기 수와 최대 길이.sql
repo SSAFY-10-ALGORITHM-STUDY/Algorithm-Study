99c87f57938ab7e7601e904e08a5ef859e670f31
kangtaekyung <rkdxorud3939@naver.com>
2024-04-11 09:57:30 +0900

    [강태경]: case문을 사용하여 null값 처리

---
+++ 알고리즘/4월 2주차/강태경/물고기 수와 최대 길이.sql
@@ -0,0 +1,8 @@
+select count(*) fish_count, max(length) max_length, fish_type
+from fish_info i
+group by fish_type
+having avg(case 
+            when length is null then 10
+           else length end
+          )>=33
+order by fish_type;
